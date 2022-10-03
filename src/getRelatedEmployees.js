a04a9cb0fe5903c2adb6077dc9c73bea0920f7fd
Vitória <vitcs11@gmail.com>
2022-10-03 20:36:18 -0300

    requisito 4

--- src/getRelatedEmployees.js
+++ src/getRelatedEmployees.js
@@ -1,11 +1,21 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function isManager(id) {
-  // seu código aqui
+  const verifyMananger = employees.some((element) =>
+    element.managers.includes(id));
+  return verifyMananger;
 }
 
 function getRelatedEmployees(managerId) {
-  // seu código aqui
+  if (isManager(managerId) === false) {
+    throw new Error('O id inserido não é de uma pessoa colaboradora gerente!');
+  }
+  const managerFilter = employees.filter(({ managers }) =>
+    managers.includes(managerId));
+  const managerFullName = managerFilter.map(
+    ({ firstName, lastName }) => `${firstName} ${lastName}`,
+  );
+  return managerFullName;
 }
 
 module.exports = { isManager, getRelatedEmployees };
