345d87b1a7d8bffc3651ad20f53682e29152661a
Vitória <vitcs11@gmail.com>
2022-10-03 19:40:57 -0300

    requisito 3

--- src/getEmployeeByName.js
+++ src/getEmployeeByName.js
@@ -1,7 +1,13 @@
-const data = require('../data/zoo_data');
+const { employees } = require('../data/zoo_data');
 
 function getEmployeeByName(employeeName) {
-  // seu código aqui
+  if (employeeName === undefined) {
+    return {};
+  }
+  const fullNameEmployee = employees.find(
+    (element) =>
+      element.firstName === employeeName || element.lastName === employeeName,
+  );
+  return fullNameEmployee;
 }
-
 module.exports = getEmployeeByName;
