1f1d395a072298f54930320080b1fdfe972a2d4a
Vitória <vitcs11@gmail.com>
2022-10-04 16:36:58 -0300

    Cobertura de 90% handlerElep

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -1,3 +1,28 @@
 const handlerElephants = require('../src/handlerElephants');
 
-describe('Testes da função HandlerElephants', () => {});
+describe('Testes da função HandlerElephants', () => {
+  it('Para o argumento count deve retornar o número inteiro 4;', () => {
+    expect(handlerElephants('count')).toBe(4);
+  });
+
+  it('Para o argumento names deve retornar um array de nomes que possui o nome Jefferson;', () => {
+    expect(handlerElephants('names')).toEqual([
+      'Ilana',
+      'Orval',
+      'Bea',
+      'Jefferson',
+    ]);
+  });
+
+  it('Para o argumento averageAge deve retornar um número próximo a 10.5;', () => {
+    expect(handlerElephants('averageAge')).toEqual(10.5);
+  });
+
+  it('Para o argumento location deve retornar a string NW;', () => {
+    expect(handlerElephants('location')).toBe('NW');
+  });
+
+  it('Não passando argumentos a função deve retornar undefined;', () => {
+    expect(handlerElephants(' ')).toBe(null);
+  });
+});
