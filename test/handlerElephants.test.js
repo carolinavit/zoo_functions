3b23f8fcab16a22d28d0fa22e8db78b2e95acb5d
Vitória <vitcs11@gmail.com>
2022-10-04 18:35:20 -0300

    100% dos testes handlerElephants

--- test/handlerElephants.test.js
+++ test/handlerElephants.test.js
@@ -23,6 +23,22 @@ describe('Testes da função HandlerElephants', () => {
   });
 
   it('Não passando argumentos a função deve retornar undefined;', () => {
+    expect(handlerElephants()).toBeUndefined();
+  });
+
+  it('Para o argumento popularity deve retornar um número igual ou maior a 5;', () => {
+    expect(handlerElephants('popularity')).toBe(5);
+  });
+
+  it('Para o argumento availability deve retornar um array de dias da semana que não contém Monday;', () => {
+    expect(handlerElephants('availability')).toEqual(['Friday', 'Saturday', 'Sunday', 'Tuesday']);
+  });
+
+  it('Passando por argumento um objeto vazio ({}) deve retornar erro', () => {
+    expect(handlerElephants(1)).toBe('Parâmetro inválido, é necessário uma string');
+  });
+
+  it('Passada uma string que não contempla uma funcionalidade deve retornar null.', () => {
     expect(handlerElephants(' ')).toBe(null);
   });
 });
