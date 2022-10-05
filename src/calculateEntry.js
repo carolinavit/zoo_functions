013ea4dd07147be81952cf4f5fa928ca5349fd4f
Vitória <vitcs11@gmail.com>
2022-10-04 21:51:07 -0300

    requisito 8

--- src/calculateEntry.js
+++ src/calculateEntry.js
@@ -1,11 +1,23 @@
 const data = require('../data/zoo_data');
+const { prices } = require('../data/zoo_data');
 
 function countEntrants(entrants) {
-  // seu código aqui
+  const filterChild = entrants.filter((element) => element.age < 18).length;
+  const filterAdult = entrants.filter((element) => element.age >= 18 && element.age < 50).length;
+  const filterSenior = entrants.filter((element) => element.age >= 50).length;
+  return {
+    child: filterChild,
+    adult: filterAdult,
+    senior: filterSenior,
+  };
 }
-
 function calculateEntry(entrants) {
-  // seu código aqui
+  if (!entrants || Object.keys(entrants).length === 0) {
+    return 0;
+  }
+  const { adult, senior, child } = prices;
+  const resultEntrants = Object.values(countEntrants(entrants));
+  return (child * resultEntrants[0]) + (adult * resultEntrants[1]) + (senior * resultEntrants[2]);
 }
 
 module.exports = { calculateEntry, countEntrants };
