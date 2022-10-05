d825643f88bb8d112bcaad2d6238056be0b02d1a
Vitória <vitcs11@gmail.com>
2022-10-05 11:07:27 -0300

    Requisito 10

--- src/getOldestFromFirstSpecies.js
+++ src/getOldestFromFirstSpecies.js
@@ -1,7 +1,20 @@
 const data = require('../data/zoo_data');
 
+const { species, employees } = data;
+
 function getOldestFromFirstSpecies(id) {
-  // seu código aqui
+  const firstSpecie = employees.find((element) => element.id === id).responsibleFor[0];
+  const residentSpecie = species.find((index) => index.id === firstSpecie).residents;
+  let oldestAge = 0;
+  let oldestInfo = [];
+  residentSpecie.forEach(({ name, sex, age }) => {
+    if (age > oldestAge) {
+      oldestAge = age;
+      oldestInfo = [name, sex, age];
+    }
+  });
+  return oldestInfo;
 }
 
+console.log(getOldestFromFirstSpecies('c5b83cb3-a451-49e2-ac45-ff3f54fbe7e1'));
 module.exports = getOldestFromFirstSpecies;
