10cdde70087a61958b4f3ff27eecaf3f55f19dca
Vitória <vitcs11@gmail.com>
2022-10-05 12:50:26 -0300

    requisito 5

--- src/countAnimals.js
+++ src/countAnimals.js
@@ -1,7 +1,34 @@
 const data = require('../data/zoo_data');
+const { species } = require('../data/zoo_data');
 
-function countAnimals(animal) {
-  // seu código aquii
+function resultAnimalSex(animal) {
+  let count = 0;
+  let getResidents = [];
+  species.forEach((element) => {
+    if (element.name === animal.specie) {
+      getResidents = element.residents;
+    }
+  });
+  getResidents.forEach((index) => {
+    if (index.sex === animal.sex) {
+      count += 1;
+    }
+  });
+  return count;
 }
 
+function countAnimals(animal) {
+  const resultCount = {};
+  species.forEach(({ name, residents }) => {
+    resultCount[name] = residents.length;
+  });
+  if (animal === undefined) {
+    return resultCount;
+  }
+  if (animal.sex) {
+    return resultAnimalSex(animal);
+  }
+  return resultCount[animal.specie];
+}
+countAnimals();
 module.exports = countAnimals;
